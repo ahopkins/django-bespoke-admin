18675b6ac8cf65a99d44250964e026bb2a2e8b9d
Adam Hopkins <adam@optymizer.com>
2016-11-29 10:58:09 +0200

    Readme and setup.py

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # django-bespoke-admin
+
+This package aims to achieve two goals:
+
+1. Allow for the addition of arbitrary views to the Django admin panel; and
+2. Create a methodology to allow for [custom management commands](https://docs.djangoproject.com/en/1.10/howto/custom-management-commands/) to be executed from the Django admin panel.
+
+**This is still in development. While operational, it is not yet sufficiently abstracted to be usable for all use cases.**
+
+You can install it by: `pip install django-bespoke-admin`
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'django-bespoke-admin',
+  packages = ['django-bespoke-admin'],
+  version = '0.1',
+  description = 'Custom views and management command execution in Django admin panel.',
+  author = 'Adam Hopkins',
+  author_email = 'adam@optymizer.com',
+  url = 'https://github.com/ahopkins/django-bespoke-admin',
+  download_url = 'https://github.com/ahopkins/django-bespoke-admin/tarball/0.1',
+  keywords = ['django', 'django admin', 'custom views', 'management commands'],
+  classifiers = [],
+)
